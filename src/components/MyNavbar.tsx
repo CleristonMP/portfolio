b3848e6894e2e270e5fd06cffe0a7b58c7509b53
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-12-03 16:07:28 -0300

    Performance and SEO adjustments

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -17,7 +17,10 @@ const MyNavbar: React.FC = () => {
 
   const BASE_URL = process.env.PUBLIC_URL || "";
 
-  const sections = useMemo(() => ["home", "projects", "about", "skills", "courses"], [])
+  const sections = useMemo(
+    () => ["home", "projects", "about", "skills", "courses"],
+    []
+  );
 
   const handleLinkClick = (section: string) => {
     const element = document.getElementById(section);
@@ -32,7 +35,7 @@ const MyNavbar: React.FC = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
-  
+
       if (scrollPosition < 50) {
         setActiveSection("home");
         const newUrl = `${BASE_URL}/#home`;
@@ -52,52 +55,126 @@ const MyNavbar: React.FC = () => {
       }
       setWindowScrollY(scrollPosition);
     };
-  
+
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, [sections]);
 
   return (
-    <header className="header-area" id="header">
-      <Navbar
-        className={`navbar navbar-expand-md fixed-top main-menu ${
-          screenSize.width >= 768
-            ? `bg-white px-sm-3 ${
-                windowScrollY >= 130 ? "nav-shadow" : "no-shadow"
-              }`
-            : "navbar-dark bg-dark px-sm-3"
-        }`}
-        expand="lg"
-      >
-        <Navbar.Brand onClick={() => handleLinkClick("home")}>
-          <img 
-          src={`${MEDIAS_BASE_URL}/logo.svg`}
-          alt="Cleriston" 
-          width={140} />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <FontAwesomeIcon icon={faBars} />
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav className="ml-auto">
-            {sections.map((section) => (
-              <Nav.Link
-                key={section}
-                as="div"
-                className={`mp-navlink ${
-                  activeSection === section ? "active" : ""
-                }`}
-                onClick={() => handleLinkClick(section)}
-              >
-                {t(`navigation.${section}`)}
-              </Nav.Link>
-            ))}
-            <ThemeSwitch />
-            <LanguageSwitcher />
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </header>
+    <>
+      <style>
+        {`
+          .header-area {
+            width: 100%;
+          }
+
+          .main-menu {
+            width: 100%;
+            max-width: 900px;
+            margin: 0 auto;
+          }
+
+          .no-shadow {
+            box-shadow: none;
+            transition: all 1s ease 0s;
+          }
+
+          .nav-shadow {
+            box-shadow: 0px 10px 30px var(--primary-shadow);
+            transition: all 1s ease 0s;
+          }
+
+          .navbar-brand {
+            padding: 10px;
+          }
+
+          .navbar-toggler {
+            border: none;
+            margin-right: 10px;
+          }
+
+          .navbar-toggler svg {
+            width: 25px;
+            height: auto;
+          }
+
+          .navbar-toggler svg path {
+            fill: var(--white-color);
+          }
+
+          .navbar-brand, .nav-link {
+            cursor: pointer;
+          }
+
+          .nav-link {
+            padding: 10px;
+          }
+
+          .navbar-nav .mp-navlink.nav-link {
+            color: var(--primary-color) !important;
+            font-weight: 500;
+          }
+
+          .navbar-nav .nav-link:hover {
+            font-weight: 700;
+          }
+
+          .navbar-nav .nav-link.active {
+            font-weight: 700;
+          }
+
+          @media (min-width: 768px) {
+            .header-area .main-menu {
+              background-color: var(--bg-color) !important;
+            }
+          }
+        `}
+      </style>
+      <header className="header-area" id="header">
+        <Navbar
+          className={`navbar navbar-expand-md fixed-top main-menu ${
+            screenSize.width >= 768
+              ? `bg-white px-sm-3 ${
+                  windowScrollY >= 130 ? "nav-shadow" : "no-shadow"
+                }`
+              : "navbar-dark bg-dark px-sm-3"
+          }`}
+          expand="lg"
+        >
+          <Navbar.Brand onClick={() => handleLinkClick("home")}>
+            <img
+              src={`${MEDIAS_BASE_URL}/logo.svg`}
+              alt="Cleriston"
+              width={140}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <FontAwesomeIcon icon={faBars} />
+          </Navbar.Toggle>
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav className="ml-auto">
+              {sections.map((section) => (
+                <Nav.Link
+                  key={section}
+                  as="div"
+                  className={`mp-navlink ${
+                    activeSection === section ? "active" : ""
+                  }`}
+                  onClick={() => handleLinkClick(section)}
+                >
+                  {t(`navigation.${section}`)}
+                </Nav.Link>
+              ))}
+              <ThemeSwitch />
+              <LanguageSwitcher />
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+    </>
   );
 };
 
