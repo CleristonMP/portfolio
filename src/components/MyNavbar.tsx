b6dded70fd5aaef25cba6fe039d099f359920f3c
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2025-05-14 11:34:30 -0300

    Navbar adjustments

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState, useCallback } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -7,6 +7,7 @@ import ThemeSwitch from "./ThemeSwitch";
 import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "./LanguageSwitcher";
 import MEDIAS_BASE_URL from "../utils/mediasBaseUrl";
+import { set, throttle } from "lodash";
 import "../styles/MyNavbar.css";
 
 const MyNavbar: React.FC = () => {
@@ -22,159 +23,94 @@ const MyNavbar: React.FC = () => {
     []
   );
 
+  const updateUrl = useCallback(
+    (section: string) => {
+      const newUrl = `${BASE_URL}/#${section}`;
+      window.history.pushState(null, "", newUrl);
+    },
+    [BASE_URL]
+  );
+
   const handleLinkClick = (section: string) => {
     const element = document.getElementById(section);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
-      setActiveSection(section);
-      const newUrl = `${BASE_URL}/#${section}`;
-      window.history.pushState(null, "", newUrl);
+      updateUrl(section);
     }
   };
 
   useEffect(() => {
-    const handleScroll = () => {
+    const handleScroll = throttle(() => {
       const scrollPosition = window.scrollY;
 
       if (scrollPosition < 50) {
         setActiveSection("home");
-        const newUrl = `${BASE_URL}/#home`;
-        window.history.pushState(null, "", newUrl);
+        updateUrl("home");
       } else {
         sections.forEach((section) => {
           const element = document.getElementById(section);
           if (element) {
             const rect = element.getBoundingClientRect();
             if (rect.top >= 0 && rect.top < window.innerHeight / 2) {
-              setActiveSection(section);
-              const newUrl = `${BASE_URL}/#${section}`;
-              window.history.pushState(null, "", newUrl);
+              setActiveSection((prevSection) => {
+                if (prevSection !== section) {
+                  updateUrl(section);
+                }
+                return section;
+              });
             }
           }
         });
       }
       setWindowScrollY(scrollPosition);
-    };
+    }, 100);
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [sections]);
+  }, [sections, updateUrl]);
 
   return (
-    <>
-      <style>
-        {`
-          .header-area {
-            width: 100%;
-          }
-
-          .main-menu {
-            width: 100%;
-            max-width: 900px;
-            margin: 0 auto;
-          }
-
-          .no-shadow {
-            box-shadow: none;
-            transition: all 1s ease 0s;
-          }
-
-          .nav-shadow {
-            box-shadow: 0px 10px 30px var(--primary-shadow);
-            transition: all 1s ease 0s;
-          }
-
-          .navbar-brand {
-            padding: 10px;
-          }
-
-          .navbar-toggler {
-            border: none;
-            margin-right: 10px;
-          }
-
-          .navbar-toggler svg {
-            width: 25px;
-            height: auto;
-          }
-
-          .navbar-toggler svg path {
-            fill: var(--white-color);
-          }
-
-          .navbar-brand, .nav-link {
-            cursor: pointer;
-          }
-
-          .nav-link {
-            padding: 10px;
-          }
-
-          .navbar-nav .mp-navlink.nav-link {
-            color: var(--primary-color) !important;
-            font-weight: 500;
-          }
-
-          .navbar-nav .nav-link:hover {
-            font-weight: 700;
-          }
-
-          .navbar-nav .nav-link.active {
-            font-weight: 700;
-          }
-
-          @media (min-width: 768px) {
-            .header-area .main-menu {
-              background-color: var(--bg-color) !important;
-            }
-          }
-        `}
-      </style>
-      <header className="header-area" id="header">
-        <Navbar
-          className={`navbar navbar-expand-md fixed-top main-menu ${
-            screenSize.width >= 768
-              ? `bg-white px-sm-3 ${
-                  windowScrollY >= 130 ? "nav-shadow" : "no-shadow"
-                }`
-              : "navbar-dark bg-dark px-sm-3"
-          }`}
-          expand="lg"
-        >
-          <Navbar.Brand onClick={() => handleLinkClick("home")}>
-            <img
-              src={`${MEDIAS_BASE_URL}/logo.svg`}
-              alt="Cleriston"
-              width={140}
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <FontAwesomeIcon icon={faBars} />
-          </Navbar.Toggle>
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-end"
-          >
-            <Nav className="ml-auto">
-              {sections.map((section) => (
-                <Nav.Link
-                  key={section}
-                  className={`mp-navlink ${
-                    activeSection === section ? "active" : ""
-                  }`}
-                  onClick={() => handleLinkClick(section)}
-                  href={`#${section}`}
-                >
-                  {t(`navigation.${section}`)}
-                </Nav.Link>
-              ))}
-              <ThemeSwitch />
-              <LanguageSwitcher />
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </header>
-    </>
+    <header className="header-area" id="header">
+      <Navbar
+        className={`navbar navbar-expand-md fixed-top main-menu ${
+          screenSize.width >= 768
+            ? `bg-white px-sm-3 ${
+                windowScrollY >= 130 ? "nav-shadow" : "no-shadow"
+              }`
+            : "navbar-dark bg-dark px-sm-3"
+        }`}
+        expand="lg"
+      >
+        <Navbar.Brand onClick={() => handleLinkClick("home")}>
+          <img
+            src={`${MEDIAS_BASE_URL}/logo.svg`}
+            alt="Cleriston"
+            width={140}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <FontAwesomeIcon icon={faBars} />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="ml-auto">
+            {sections.map((section) => (
+              <Nav.Link
+                key={section}
+                className={`mp-navlink ${
+                  activeSection === section ? "active" : ""
+                }`}
+                onClick={() => handleLinkClick(section)}
+                href={`#${section}`}
+              >
+                {t(`navigation.${section}`)}
+              </Nav.Link>
+            ))}
+            <ThemeSwitch />
+            <LanguageSwitcher />
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
   );
 };
 
